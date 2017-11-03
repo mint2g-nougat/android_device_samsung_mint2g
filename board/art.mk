699924bd29f709f040d5538bfe09145033dbe319
Cswl Coldwind <cswl1337@gmail.com>
2017-11-03 12:14:35 +0545

    board: ART add low ram and single processor flag

--- board/art.mk
+++ board/art.mk
@@ -1,3 +1,9 @@
+# Low ram device
+TARGET_LOW_RAM_DEVICE := true
+
 # Art coonfigurations
 WITH_DEXPREOPT := true
 DONT_DEXPREOPT_PREBUILTS := true
+
+# Single processor
+DEX2OAT_TARGET_INSTRUCTION_SET_FEATURES := -smp
