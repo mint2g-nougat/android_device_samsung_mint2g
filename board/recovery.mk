3a1a6c39cbf3922952bd7f2add2e6e92d352c6ad
Cswl Coldwind <cswl1337@gmail.com>
2017-11-03 15:46:05 +0545

    recovery: delete obsolete flag custom recovery font

--- board/recovery.mk
+++ board/recovery.mk
@@ -2,7 +2,6 @@
 DEVICE_RESOLUTION := 240x320
 BOARD_LDPI_RECOVERY := true
 BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/mint2g/recovery/recovery_keys.c
 BOARD_HAS_NO_MISC_PARTITION := true
 TARGET_RECOVERY_FSTAB := device/samsung/mint2g/recovery.fstab
